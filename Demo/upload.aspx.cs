84fdff722141f282c85b1031568959210c65c000
阿星Plus <123@meowv.com>
2016-11-22 11:22:14 +0800

    图片批量上传
    图片批量上传

---
+++ Demo/upload.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demo
+{
+    public partial class upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpFileCollection files = Request.Files;
+            if (files.Count == 0)
+            {
+                Response.Write("错误请求，未检查到客户端提交的文件信息！");
+                Response.End();
+            }
+
+
+            string path = Server.MapPath("UploadImagesList");
+
+            HttpPostedFile file = files[0];
+
+            if (file != null && file.ContentLength > 0)
+            {
+                string savePath = path + "/" + Guid.NewGuid().ToString() + "_" + Request.Form["fileName"];
+                file.SaveAs(savePath);
+            }
+        }
+    }
+}
